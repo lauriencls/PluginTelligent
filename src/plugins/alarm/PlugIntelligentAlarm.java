9ffd513b1190e3954f0935d9a2deef036d933bf6
lauriencls <laurie.divers@gmail.com>
2018-05-23 17:29:30 +0200

    Ajout méthodes AlarmLoader + Modif date de rappel
    Ajout contrôle si modification de la date de rappel
    Définition des méthodes de AlarmLoader dans AlarmLoader et plus dans
    PluginTelligentAlarm

--- src/plugins/alarm/PlugIntelligentAlarm.java
+++ src/plugins/alarm/PlugIntelligentAlarm.java
@@ -1,43 +1,23 @@
 package plugins.alarm;
 
-import java.awt.Frame;
 import java.awt.Toolkit;
 import java.time.Clock;
 import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
-import core.UI.Body;
 import core.model.AlarmLoader;
 import model.Alarm;
 import model.Message;
 
 public class PlugIntelligentAlarm extends AlarmLoader {
-
-	Body body;
 	
 	/**
 	 * Constructeur de la classe
 	 */
 	public PlugIntelligentAlarm() {
 		super();
-	}
-
-	@Override
-	public Alarm createAlarm(Message message, Instant triggerDateTime, boolean isActive) {
-		Alarm alarm = new Alarm(message, triggerDateTime, isActive);
-		triggerAlarm(alarm);
-		return alarm;
-	}
-
-	@Override
-	public void setupAlarmTriggerDate(Alarm alarm, Instant triggerDateTime) {
-			alarm.setTriggerDateTime(triggerDateTime);
-	}
+	}	
 
 	@Override
 	public void triggerAlarm(Alarm alarm) {
@@ -46,6 +26,7 @@ public class PlugIntelligentAlarm extends AlarmLoader {
 		if(!alarm.getTriggerDateTime().isBefore(Instant.now(Clock.systemUTC()))) {
 			while(!alarm.getTriggerDateTime().equals(Instant.now(Clock.systemUTC())))
 			{
+				//On attend d'avoir atteint la date à laquelle l'alarme doit sonner
 			}
 					
 			//Sonnerie de l'alarme
@@ -53,18 +34,9 @@ public class PlugIntelligentAlarm extends AlarmLoader {
 			System.out.println(alarm.getMessage().toString());
 			JFrame frame = new JFrame();
 			JOptionPane.showMessageDialog(frame, alarm.getMessage().getTitle() + " - " + alarm.getMessage().getBody(), "RAPPEL", JOptionPane.PLAIN_MESSAGE);
-			t.stop();	
 		}
 		
 		
-	}
-
-	@Override
-	public void changeActivationAlarm(Alarm alarm) {
-		alarm.setActive(!alarm.isActive());
-		if(alarm.isActive()) {
-			triggerAlarm(alarm);
-		}
 	}
 	
 	public static void main(String[] args){
